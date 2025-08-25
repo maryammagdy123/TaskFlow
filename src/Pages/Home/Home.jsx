a55616038bbd27899bd8e1782a59c8fa874383e1
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2025-08-25 03:08:40 +0300

    remove unused flowbite components import

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { Button, Progress, Textarea } from 'flowbite-react'
+import {  Progress } from 'flowbite-react'
 import AddTaskForm from '../../Components/AddTaskForm/AddTaskForm'
 import { useContext } from 'react'
 import { TaskContext } from '../../Context/TasksContextProvider'
